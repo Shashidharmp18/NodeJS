b104d634c0e8b42a6fa718dc1448c56ad2911088
MP Shashi <shashidharpatgar8@gmail.com>
2024-06-27 23:58:20 +0530

    first commit

--- file.js
+++ file.js
@@ -1,8 +1,25 @@
+const { getEventListeners } = require("events");
 const fs = require("fs");
 
-//fs.writeFileSync("./test.txt","Hey there")
+
+fs.writeFileSync("./test.txt","Hey there")
 
 
 const result=fs.readFileSync('./test.txt','utf-8');
-
 console.log(result);
+
+fs.readFile('./test.txt','utf-8',(err, result)=>{
+    if(err){
+        console.log("error", err);
+    }else{
+        console.log(result);
+    }
+})
+
+fs.appendFileSync('./test.txt',`\nhey there\n`);
+
+fs.copyFileSync('./test.txt','copy.txt');
+  
+fs.unlinkSync('./copy.txt');
+
+console.log(fs.statSync('./test.txt').isFile());
