ef96fcbe00cdc32a5645c34223df83685787cd78
MP Shashi <shashidharpatgar8@gmail.com>
2024-06-27 15:54:08 +0530

    first commit

--- file.js
+++ file.js
@@ -1,3 +1,8 @@
 const fs = require("fs");
 
-fs.writeFileSync("./test.txt","Hey there")
+//fs.writeFileSync("./test.txt","Hey there")
+
+
+const result=fs.readFileSync('./test.txt','utf-8');
+
+console.log(result);
